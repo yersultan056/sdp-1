4ecc536ec4c9eabd181c563fafc9072542dbafb1
yersultan056 <230129@astanait.edu.kz>
2024-10-24 14:39:22 +0500

    Task 4

--- src/Assignment_4/Task_3/Demo.java
+++ src/Assignment_4/Task_3/Demo.java
@@ -2,16 +2,13 @@ package Assignment_4.Task_3;
 
 public class Demo {
     public static void main(String[] args) {
-        PricingStrategy distanceBased = new DistanceBasedPricing(250);
-        Trip trip1 = new Trip(distanceBased);
+        Trip trip1 = new Trip(new DistanceBasedPricing(250));
         System.out.println("Cost by distance: " + trip1.calculateTripCost(10, 20));
 
-        PricingStrategy timeBased = new TimeBasedPricing(150);
-        trip1.setPricingStrategy(timeBased);
+        trip1.setPricingStrategy(new TimeBasedPricing(150));
         System.out.println("Cost by time: " + trip1.calculateTripCost(10, 20));
 
-        PricingStrategy fixedPrice = new FixedPricePricing(2500);
-        trip1.setPricingStrategy(fixedPrice);
+        trip1.setPricingStrategy(new FixedPricePricing(2500));
         System.out.println("Fixed price: " + trip1.calculateTripCost(10, 20));
     }
 }
